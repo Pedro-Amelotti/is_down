b2da5c44f0fbcc3e0da21bc3bbd4c96df55a1bae
Pedro <pedro.vamelotti@gmail.com>
2025-10-06 09:56:30 -0300

    feat: enviar alertas de indisponibilidade via discord

--- status_monitor/status_monitor/settings.py
+++ status_monitor/status_monitor/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +127,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Integrações externas
+DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
